82e92f3c593261de14c876249446fa9182aaa8f8
JAZzmiX <mr.jazzmix@gmail.com>
2025-05-09 16:22:35 +0300

    init

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -6,10 +6,12 @@ import { JoinCommunityBlock } from "./components/JoinCommunityBlock";
 import { ProudFiguresBlock } from "./components/ProudFiguresBlock";
 import { RoadmapBlock } from "./components/RoadmapBlock/";
 import { TeamBlock } from "./components/TeamBlock";
+import { HeroBlock } from '@/components/pages/HomePage/components/HeroBlock';
 
 export const HomePage = () => {
   return (
     <>
+      <HeroBlock />
       <AboutBlock />
       <ProudFiguresBlock />
       <TeamBlock />
