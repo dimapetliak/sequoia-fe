99a3914e475ad838f020d2dc479e058e1c5f6a50
Dmytro Petliak <dmytro.petliak@patrianna.com>
2025-05-10 12:40:22 +0300

    [no-ticket]: added path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const path = process.env.NODE_ENV === "production" ? "/sequoia-fe" : "";
+
 const nextConfig: NextConfig = {
+  basePath: path,
   output: "export",
   distDir: "dist",
   images: {
