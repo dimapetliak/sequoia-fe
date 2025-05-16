edaa4d9626c7e098ae8fb796d81219d9745f1367
JAZzmiX <mr.jazzmix@gmail.com>
2025-05-16 19:44:02 +0300

    added responsive blocks

--- src/components/UI/organisms/PortfolioGrid/PortfolioGrid.tsx
+++ src/components/UI/organisms/PortfolioGrid/PortfolioGrid.tsx
@@ -1,14 +1,31 @@
+import { Typography } from '@/components/UI/atoms/Typography';
 import styles from "./styles.module.scss";
 
 export const PortfolioGrid = () => {
   return (
     <div className={styles.container}>
       <div className={styles.child1}>
-        <div className={styles.maskTop}></div>
-        <div className={styles.maskBottom}></div>
+        <Typography textAlign="left" as={"h3"} font="rattani" className={styles.title} >
+          PARK
+        </Typography>
       </div>
-      <div className={styles.child2} />
-      <div className={styles.child3} />
+      <div className={styles.child2} >
+        <Typography textAlign="left" as={"h3"} font="rattani" className={styles.title} >
+          NFT
+        </Typography>
+        <div className={styles.cutoutRightTop}></div>
+        <div className={styles.cutoutRightBottom}></div>
+        <div className={styles.cutoutLeftTop}></div>
+        <div className={styles.cutoutLeftTopMob}></div>
+        <div className={styles.cutoutLeftBottom}></div>
+        <div className={styles.cutoutLeftBottomMob}></div>
+      </div>
+      <div className={styles.child3}>
+        <Typography textAlign="left" as={"h3"} font="rattani" className={styles.title} >
+          GAME
+        </Typography>
+        <div className={styles.cutoutLeftBottom}></div>
+      </div>
     </div>
   );
 };
